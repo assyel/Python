cfc69645912b9740fde0dfd01b055f0d7e4680cd
assyel <85995602+assyel@users.noreply.github.com>
2022-01-24 15:58:15 +0700

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,20 @@
+
+
+a = 'Yolochka'
+b = 'Zelonaya'
+c = a + b
+d = 3
+#print(a + ' ' + b)
+#print(a, b)
+result = a + str(d)
+print(result)
+print(type(d))
+print(a*d)
+print(a[1]*d)
+
+a = 5
+a = 'Vadim'
+print(a)
+
+a = '5'
+print(a)
